252d69f011960b17ebfb251c8f131aabdd1cb60d
Chinnii Sushma <chininiisushma@gmail.com>
2026-04-18 17:19:17 +0530

    added input program

---
+++ user input 2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter something: ");
+        string input = Console.ReadLine();
+
+        Console.WriteLine("You entered: " + input);
+
+        Console.ReadLine();
+    }
+}
