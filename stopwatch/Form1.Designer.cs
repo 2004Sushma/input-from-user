a663f5af8b3fc207e2d8d013553ab5025407a12a
Chinnii Sushma <chininiisushma@gmail.com>
2026-04-21 11:20:36 +0530

    stopwatch executed

---
+++ stopwatch/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace stopwatch
+{
+    partial class StopWatch
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            btnstart = new Button();
+            btnstop = new Button();
+            btnresume = new Button();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // btnstart
+            // 
+            btnstart.Location = new Point(158, 403);
+            btnstart.Name = "btnstart";
+            btnstart.Size = new Size(196, 96);
+            btnstart.TabIndex = 0;
+            btnstart.Text = "START";
+            btnstart.UseVisualStyleBackColor = true;
+            btnstart.Click += button1_Click;
+            // 
+            // btnstop
+            // 
+            btnstop.Location = new Point(509, 403);
+            btnstop.Name = "btnstop";
+            btnstop.Size = new Size(196, 96);
+            btnstop.TabIndex = 1;
+            btnstop.Text = "STOP";
+            btnstop.UseVisualStyleBackColor = true;
+            btnstop.Click += button2_Click;
+            // 
+            // btnresume
+            // 
+            btnresume.Location = new Point(838, 403);
+            btnresume.Name = "btnresume";
+            btnresume.Size = new Size(196, 96);
+            btnresume.TabIndex = 2;
+            btnresume.Text = "RESUME";
+            btnresume.UseVisualStyleBackColor = true;
+            btnresume.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ButtonFace;
+            label1.Location = new Point(555, 201);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 20);
+            label1.TabIndex = 3;
+            label1.Text = "00:00:00:00";
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // StopWatch
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1182, 753);
+            Controls.Add(label1);
+            Controls.Add(btnresume);
+            Controls.Add(btnstop);
+            Controls.Add(btnstart);
+            Name = "StopWatch";
+            Text = "form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnstart;
+        private Button btnstop;
+        private Button btnresume;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ stopwatch/Form1.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Windows.Forms;
+
+namespace stopwatch
+{
+    public partial class StopWatch : Form
+    {
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
+
+        public StopWatch()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000; // 1 second
+        }
+
+        // TIMER LOGIC
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+
+            if (seconds == 60)
+            {
+                seconds = 0;
+                minutes++;
+            }
+
+            if (minutes == 60)
+            {
+                minutes = 0;
+                hours++;
+            }
+
+            label1.Text = hours.ToString("D2") + ":" +
+                          minutes.ToString("D2") + ":" +
+                          seconds.ToString("D2");
+        }
+
+        // START BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+
+            label1.Text = "00:00:00";
+            timer1.Start();
+        }
+
+        // STOP BUTTON
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        // RESUME BUTTON
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
