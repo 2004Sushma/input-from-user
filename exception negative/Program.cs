a4f8a53c3a45176124242f61a8f06887580162e8
Chinnii Sushma <chininiisushma@gmail.com>
2026-04-21 09:34:22 +0530

    negative executed

---
+++ exception negative/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class NegativeNumberException : Exception
+{
+    public NegativeNumberException(string message) : base(message) { }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        int num = int.Parse(Console.ReadLine());
+
+        try
+        {
+            if (num < 0)
+            {
+                throw new NegativeNumberException("Number should not be negative.");
+            }
+
+            Console.WriteLine("Valid number");
+        }
+        catch (NegativeNumberException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Execution completed.");
+        }
+    }
+}
