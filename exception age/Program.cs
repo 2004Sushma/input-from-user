b30994ecd7031404a171f419ae1e165bf91f02f0
Chinnii Sushma <chininiisushma@gmail.com>
2026-04-21 09:31:17 +0530

    valid age

---
+++ exception age/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+class AgeException : Exception
+{
+    public AgeException(string message) : base(message) { }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter age:");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age < 18)
+        {
+            throw new AgeException("Age must be 18 or above.");
+        }
+
+        Console.WriteLine("Valid age");
+    }
+}
