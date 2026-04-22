d21a80bd9a8e58a9221780d83e3e098b2c325a01
Chinnii Sushma <chininiisushma@gmail.com>
2026-04-22 11:53:01 +0530

    binary executed

---
+++ publisher subscriber model/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class NewsAgency
+{
+    public delegate void NewsHandler(string news);
+    public event NewsHandler PublishNews;
+
+    public void Publish(string news)
+    {
+        PublishNews?.Invoke(news);
+    }
+}
+
+class Reader
+{
+    public void ReceiveNews(string news)
+    {
+        Console.WriteLine("Received: " + news);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        NewsAgency agency = new NewsAgency();
+        Reader r1 = new Reader();
+        Reader r2 = new Reader();
+
+        agency.PublishNews += r1.ReceiveNews;
+        agency.PublishNews += r2.ReceiveNews;
+
+        agency.Publish("New update released!");
+    }
+}
